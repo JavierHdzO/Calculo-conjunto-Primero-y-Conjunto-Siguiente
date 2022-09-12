e92b9e9f4a38d24d337f62fd851585d4efb55b42
JavierHdzO <fco_hernandez1705@hotmail.com>
2022-09-11 23:47:22 -0500

    cm6 091122F Added comments to isInclideStrict inside IsInclude module

--- src/helpers/getStrings.js
+++ src/helpers/getStrings.js
@@ -12,21 +12,45 @@ export const getString = ( produccion_rules, list_variables, original_head ) => 
     const produccion_strings = {}
     for (let i = 0; i < produccion_rules.length; i++) {
 
-        
+        /**
+         * Se itera cada regla de producción para obtener aquellas cadenas que contengan a ORIGINAL_HEAD
+         * 
+         * Se otiene la varaible que se esta iterando y se nombra HEAD
+         * Se obtienen las cadenas de las reglas de producción para HEAD
+         */
         const head = produccion_rules[i][0];
         const body = produccion_rules[i][1];
 
         
+        /**
+         * Se crea un objeto que contedra cada variable con las cadenas que contengan a ORIGINAL_HEAD
+         */
         produccion_strings[head] = []
         for (let j = 0; j < body.length; j++) {
             
+            /**
+             * Regresa el indice de la varaible  que se esta buscando en la cadena
+             * retorna -1 si no se encontro la varaible en la cadena.
+             * retorna el indice si se encontro la varaible en la cadena
+             */
             if ( isIncludeStrict(body[j], original_head) != -1 ){
+                /**
+                 * En caso de ternar la varaible en la cadena se 
+                 * agrega la cadena al objeto que guarada unicamente 
+                 * las cadenas de producción para la variable ORIGINAL.
+                 * 
+                 * GUARDANDO COMO REFERENCIA DE QUE VARAIBLE SE ORIGINAL DICHA CADENA.
+                 * Esto con relacion a las regla de producción originales
+                 */
                 produccion_strings[head].push(body[j]);
             }
             
         }
 
-        
+        /**
+         * Eliman las variables que no tienen una regla de produccion que contenga a la variable
+         * que se está analizando
+         */
 
         if(produccion_strings[head].length === 0){
             delete produccion_strings[head];
--- src/helpers/isInclude.js
+++ src/helpers/isInclude.js
@@ -24,10 +24,36 @@ export const isInclude = ( cadena = "", list_var = [] ) => {
     return miIndex
 }
 
+/**
+ * 
+ * @param {*} cadena es la cadena de producción a la que se le quiere localizar si contiene una variable
+ * @param {*} variable es aquella que se quiere localizar dentro de la cadea
+ * @returns return -1 si no la cadena no contiene la variable buscada, en caso contrario se retorna el 
+ * indice de la variable
+ */
 export const isIncludeStrict = (cadena = "", variable  = "") => {
 
+    /**
+     * Esta función trara de evitar que se confunda X o X' (Otros Ej. Y con Y', Z con Z', A con A', etc) cuando se busque alguna variable 
+     */
+
+    /**Se debe obtener en INDEX el indiece de la varaible dentro e la cadena */
     const index = cadena.indexOf( variable)
     if( index != -1 ){
+
+        /** Si INDEX tiene el indice de la varaible, entonces ahora a se busca dicha variable pero concatenando ', esto porque
+         * si se manda una X, posiblemente la cadena tenga abcX'a, por lo que se podria confundir. Entonces a cada varaible se le debe buscar 
+         * su ' (prima).
+         * 
+         */
+
+        /**
+         * Se busca la ' (varaible prima) dentro de la cadena si no se encuentra debe dar -1, si se encuentra debe dar el indice de donde se 
+         * encuentre.
+         * 
+         * El indice de la varaible deber ser diferente al indice de la variable prima, esto indica que no se confunden dichas variables dentro
+         * de la cadena. Indicando que si existe la variable buscada.
+         */
         const index2 = cadena.indexOf( variable+"'" );
         if( index != index2){
             return index;
